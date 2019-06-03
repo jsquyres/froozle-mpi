5a0dbeeaafce91bf12257dda85ea6b3bd1ddbe42
Jeff Squyres <jsquyres@cisco.com>
2019-06-03 14:29:12 -0400

    fortran-mpifh: split mpif.h
    Split mpif.h into multiple pieces so that we can include individual
    parts (from other fortran-FOO subdirectories).
    
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>

---
+++ fortran-mpifh/mpif-constants.h
@@ -0,0 +1,20 @@
+! -*- f90 -*-
+!
+! Copyright (c) 2019 Cisco Systems, Inc.  All rights reserved.
+!
+
+  integer MPI_SUCCESS
+  parameter(MPI_SUCCESS = 0)
+
+  integer MPI_COMM_WORLD
+  parameter(MPI_COMM_WORLD = 0)
+
+  integer MPI_INTEGER, MPI_CHARACTER
+  parameter(MPI_INTEGER = 0)
+  parameter(MPI_CHARACTER = 1)
+
+  integer MPI_COUNT_KIND
+  parameter(MPI_COUNT_KIND = 8)
+
+  integer MPI_STATUS_SIZE
+  parameter(MPI_STATUS_SIZE = 32)
---
+++ fortran-mpifh/mpif-sentinels.h
@@ -0,0 +1,7 @@
+! -*- f90 -*-
+!
+! Copyright (c) 2019 Cisco Systems, Inc.  All rights reserved.
+!
+
+  integer, dimension(MPI_STATUS_SIZE) :: MPI_STATUS_IGNORE
+  common/froozle_mpi_status_ignore/MPI_STATUS_IGNORE
